4458022be2cfd53a84cb7f2b826c2e2675cf4a8a
Anonymus1145 <Anonymus1145@yahoo.com>
2023-12-10 18:13:53 +0200

    Update

---
+++ jest.config.ts
@@ -0,0 +1,27 @@
+import type {Config} from 'jest';
+
+const config: Config = {
+    automock: true,
+    collectCoverage: true,
+    collectCoverageFrom: [
+        'src/**/*.(ts, tsx)',
+        'src/**/*.(js, jsx)',
+        '[vendor]/**/*.(js, jsx)',
+        '!**/node_modules/**',
+    ],
+    coverageProvider: 'babel',
+    coverageThreshold: {
+        global: {
+            branches: 100,
+            functions: 100,
+            lines: 100,
+            statements: 100,
+        },
+    },
+    maxConcurrency: 5,
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    verbose: true,
+};
+
+export default config;
